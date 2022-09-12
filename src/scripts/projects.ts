417402f11901e4d9270bfd07b282dfb8e14a9027
Rishabh Jain <rishabhnjain@gmail.com>
2022-09-12 18:08:46 +0530

    basic layout

--- src/scripts/projects.ts
+++ src/scripts/projects.ts
@@ -2,6 +2,6 @@ export default () => {
   const projCardsShine = document.querySelectorAll<HTMLDivElement>(".shine");
 
   projCardsShine.forEach((el: HTMLDivElement) => {
-    el.style.setProperty("--animation-time", Math.random() * 10 * 3 + "s");
+    el.style.setProperty("--animation-time", Math.random() * 10 + "s");
   });
 };

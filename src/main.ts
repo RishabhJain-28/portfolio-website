b52bd43a05328b918bb8160758fac2952e482de8
Rishabh Jain <rishabhnjain@gmail.com>
2022-09-15 02:28:11 +0530

    landing

--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,40 @@ import expirenceComp from "./scripts/timeline";
 import projects from "./scripts/projects";
 
 async function main() {
-  await setupLoader();
+  // await setupLoader();
   headingAnimation();
+
   projects();
   expirenceComp();
+  copyEmail();
+  copyPhone();
 }
+
+const copyEmail = () => {
+  const emailCopyATag = document.getElementById("emailCopyATag");
+  const emailCopyText = document.getElementById("emailCopyText");
+
+  emailCopyATag?.addEventListener("click", () => {
+    navigator.clipboard.writeText(`rishabhnjain@gmail.com`);
+
+    emailCopyText!.innerText = "copied!";
+  });
+  emailCopyATag?.addEventListener("mouseleave", () => {
+    emailCopyText!.innerText = "copy";
+  });
+};
+const copyPhone = () => {
+  const phoneCopyATag = document.getElementById("phoneCopyATag");
+  const phoneCopyText = document.getElementById("phoneCopyText");
+
+  phoneCopyATag?.addEventListener("click", () => {
+    navigator.clipboard.writeText(`+91-9953038121`);
+
+    phoneCopyText!.innerText = "copied!";
+  });
+  phoneCopyATag?.addEventListener("mouseleave", () => {
+    phoneCopyText!.innerText = "copy";
+  });
+};
 
 main();

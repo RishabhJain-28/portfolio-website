0d1ed53c15b44e0d85129500f8fa22ab27f037e6
Rishabh Jain <rishabhnjain@gmail.com>
2022-12-09 07:30:34 +0530

    skills aniation

--- src/scripts/skills.ts
+++ src/scripts/skills.ts
@@ -30,29 +30,42 @@ const buckets = {
 export default () => {
   const skillsDiv = document.getElementById("skills");
 
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((e) => {
+      if (e.isIntersecting) {
+        e.target.classList.add("inView");
+        observer.unobserve(e.target);
+      } else {
+        e.target.classList.remove("inView");
+      }
+    });
+  });
+
   Object.values(buckets)
     .reduce((prev, curr) => {
       return [...prev, ...curr];
     }, [])
-    .forEach((ts) => {
+    .forEach((ts, i) => {
       const div = document.createElement("div");
+      div.id = "skill";
+      div.style.transitionDelay = `${i * 50}ms`;
       const show = () => {
         const tag = div.getElementsByTagName("h1")[0];
-        tag.className = "block";
+        tag.classList.remove("tab:hidden");
 
         setTimeout(() => {
-          tag.className = "tab:hidden";
+          tag.classList.add("tab:hidden");
         }, 2000);
       };
       div.addEventListener("click", show);
 
-      div.className = `cursor-pointer w-17 border-white rounded-full p-2 px-4 flex items-center m-2 text-xl xlscreen:text-3xl gap-3`;
+      div.className = `tab:cursor-pointer w-17 border-white rounded-full p-2 px-4 flex items-center m-2 text-xl xlscreen:text-3xl gap-3`;
 
       div.innerHTML = `
         <img src="${teckstackItemImage[ts]}" class=" lg:w-8 w-7 ">
-        <h1 class="tab:hidden">${ts}</h1>
+        <h1 class=" tab:hidden">${ts}</h1>
         `;
-
       skillsDiv?.appendChild(div);
+      observer.observe(div);
     });
 };

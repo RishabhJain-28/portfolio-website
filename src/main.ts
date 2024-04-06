27a252e25be419f2420e1e5eeaaf81610d34d82a
Rishabh Jain <rishabhnjain@gmail.com>
2024-04-07 04:35:54 +0530

    copy email rename

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,23 @@
 import "./styles/index.css";
 import "./styles/header.css";
-// import "./styles/loader.css";
 import "./styles/projects.css";
 
 import setupLoader from "./scripts/loader";
 import headingAnimation from "./scripts/heading";
 import expirenceComp from "./scripts/timeline";
-// import projects from "./scripts/projects";
 import skills from "./scripts/skills";
 import project_spline from "./scripts/projects_spline";
 
 async function main() {
   project_spline();
   await setupLoader();
-  console.log(1);
   headingAnimation();
   skills();
   expirenceComp();
-  copyEmail();
+  setupCopyEmail();
 }
 
-const copyEmail = () => {
+const setupCopyEmail = () => {
   const emailCopyATag = document.getElementById("emailCopyATag");
   const emailCopyText = document.getElementById("emailCopyText");
 
